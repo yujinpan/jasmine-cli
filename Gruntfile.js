1360f0432499d6abc2da6b7c94d905206b70b68f
yujinpan <yujinpan@cmstop.com>
2017-11-30 15:18:39 +0800

    增加testem配置,搭建第一个测试项

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,26 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-
+        testem: {
+            unit: {
+                options: {
+                    framework: 'jasmine2',
+                    launch_in_dev: ['PhantomJS'],
+                    serve_files: [
+                        'src/**/*.js',
+                        'test/**/*.js'
+                    ],
+                    watch_files: [
+                        'src/**/*.js',
+                        'test/**/*.js'
+                    ]
+                }
+            }
+        }
     });
+
+    grunt.loadNpmTasks("grunt-testem-mincer");
+
+    grunt.registerTask("default", ['testem:run:unit']);
 
 };
