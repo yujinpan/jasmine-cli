78675db081a0b4ebad458020eab0ac15968b8214
yujinpan <yujinpan@cmstop.com>
2017-12-01 10:42:11 +0800

    Watch Object Properties:$watch And $digest

--- test/scope.spec.js
+++ test/scope.spec.js
@@ -2,13 +2,32 @@
 /* global Scope: false */
 'use strict';
 
-describe("Scope", function() {
+describe("Scope", function () {
 
-    it("可以被构建并用作对象", function() {
-        var scope = new Scope();
+    var scope;
+
+    beforeEach(function () {
+        scope = new Scope();
+    });
+
+    it("可以被构建并用作对象", function () {
         scope.aProperty = 1;
 
         expect(scope.aProperty).toBe(1);
+    });
+
+    describe("Digest", function () {
+
+        it("在$digest时能调用监听器函数", function () {
+            var watchFn = function () { return 'wat'; };
+            var listenerFn = jasmine.createSpy();
+            scope.$watch(watchFn, listenerFn);
+
+            scope.$digest();
+
+            expect(listenerFn).toHaveBeenCalled();
+        });
+
     });
 
 });
